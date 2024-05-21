11ad882e0ed344ccc2a29455ec02b2da3f637e4c
raviraj66 <rajanrajrr666666@gmail.com>
2024-05-21 16:58:21 +0530

    files are upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Component/Navbar/Navbar'
+import Hero from './Component/Hero/Hero'
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className='hero-text'>
+        <h1>If you can imagine it , we can
+          <br /> build it.</h1>
+        <p>We are a people-oriented,progressive business,
+          driven by our values to deliver and lasting change for 
+          our communites.
+        </p>
+        <button className='btn'>Explore more <img src={dark_arrow} alt='' /></button>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
