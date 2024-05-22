52bb3c52a6c73e642c0eb19cdbf170a8c947ed86
raviraj66 <rajanrajrr666666@gmail.com>
2024-05-22 17:29:14 +0530

    add servie and work

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Component/Navbar/Navbar'
 import Hero from './Component/Hero/Hero'
 import About from './Component/About/About'
 import Services from './Component/Services/Services'
+import MyWork from './Component/MyWork/MyWork'
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Hero/>
       <About/>
       <Services/>
+      <MyWork/>
       
     </div>
   )
--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,33 +1,33 @@
 const Services_Data = [
     {
         s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"New Building Construction",
+        s_desc:"Whatever your construction requirements we can help you - from new builds to refurbishments."
     },
     {
         s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Architectural Designs",
+        s_desc:"Building Design, Interior Design, Structural Design, 3D Visualization."
     },
     {
         s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Commercial and Electrical Installations",
+        s_desc:"We install and cable associated devices such as switches, distribution boards, sockets, and light fittings in a structure.."
     },
     {
         s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Landscaping & External Works",
+        s_desc:"Hard landscaping projects create an interesting outside area to your property and will add to its value."
     },
     {
         s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Mechanical and Renewables",
+        s_desc:"We design, install, commission, maintain and service a vast range of mechanical equipment in Private Housing, Social Housing and Commercial properties."
     },
     {
         s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Roofing Installation",
+        s_desc:"We provide effective, service-oriented roofing and consistently providing our clients with materials of the highest quality."
     },
 ]
  
